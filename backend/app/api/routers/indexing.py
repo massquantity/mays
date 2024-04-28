363900103aa3cb40380820a07a094d51ea911e55
massquantity <wdmjjxg@163.com>
2024-04-28 22:32:56 +0800

    chore(backend): save upload file

--- backend/app/api/routers/indexing.py
+++ backend/app/api/routers/indexing.py
@@ -3,22 +3,15 @@ import logging
 from pathlib import Path
 
 from fastapi import APIRouter
-from llama_index.core import (
-    SimpleDirectoryReader,
-    StorageContext,
-    VectorStoreIndex,
-    load_index_from_storage,
-)
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
 from pydantic import BaseModel
 
 from ...utils import (
     DATA_DIR,
     PERSIST_DIR,
     check_api_key,
-    clear_index_dir,
-    create_save_dirs,
+    create_dir,
     global_model_settings,
-    is_index_empty,
 )
 
 check_api_key()
@@ -43,23 +36,13 @@ def save_file(request: UploadRequest):
     return file_path
 
 
-def load_index():
-    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
-    return load_index_from_storage(storage_context)
-
-
+# todo: add new file, test OpenAI model
 @router.post("")
 async def indexing(request: UploadRequest):
     global_model_settings()
-    create_save_dirs()
+    create_dir()
     file_path = save_file(request)
     documents = SimpleDirectoryReader(input_files=[file_path]).load_data()
-    if is_index_empty():
-        index = VectorStoreIndex.from_documents(documents, show_progress=True)
-    else:
-        index = load_index()
-        for doc in documents:
-            index.insert(doc, show_progress=True)
-        clear_index_dir()
+    index = VectorStoreIndex.from_documents(documents, show_progress=True)
     index.storage_context.persist(persist_dir=PERSIST_DIR)
     logger.info(f"Finished indexing to {PERSIST_DIR}...")
