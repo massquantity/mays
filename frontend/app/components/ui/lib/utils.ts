86ecbd883785f3fb6080ac7c15a7026d65a7e7e3
massquantity <wdmjjxg@163.com>
2024-04-29 00:28:39 +0800

    feat(frontend): fetch with timeout (#6)

--- frontend/app/components/ui/lib/utils.ts
+++ frontend/app/components/ui/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+interface FetchOptions {
+  method: string;
+  headers?: Record<string, string>;
+  body: string;
+  timeout?: number;
+}
+
+export async function fetchWIthTimeout(url: string, options: FetchOptions) {
+  const { timeout = 10_000 } = options;
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+  const response = await fetch(url, {
+    ...options,
+    signal: controller.signal,
+  });
+  clearTimeout(id);
+  return response;
+}
