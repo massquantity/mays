dedaede7547f509dc9101136a4cd3351fd824e40
massquantity <wdmjjxg@163.com>
2024-05-03 11:02:12 +0800

    feat(frontend): add header (#9)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-import './globals.css';
+import '@/app/globals.css';
+import { Providers } from '@/components/providers';
+import { Header } from '@/components/header';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Chat with LLM',
-  description: 'Chat with LLM',
+  title: 'Chat with Files',
+  description: 'Chat with Files',
 };
 
 export default function RootLayout({
@@ -18,7 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <div className="flex min-h-screen flex-col">
+            <Header />
+            <main className="flex flex-1 flex-col bg-muted/50">{children}</main>
+          </div>
+        </Providers>
+      </body>
     </html>
   );
 }
