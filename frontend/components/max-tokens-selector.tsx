a0cfa00e35961196eadd7c3c2e41b115a600d955
massquantity <wdmjjxg@163.com>
2024-08-07 22:41:47 +0800

    feat(frontend): add parameter hover-card

--- frontend/components/max-tokens-selector.tsx
+++ frontend/components/max-tokens-selector.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card';
 import { Label } from '@/components/ui/label';
 import { Slider } from '@/components/ui/slider';
 import { useParamStore } from '@/lib/param-store';
@@ -18,14 +19,22 @@ export default function MaxTokensSelector() {
 
   return (
     <div className="flex flex-col space-y-2 lg:max-w-[250px] xl:max-w-[400px]">
-      <div className="flex items-center justify-between">
-        <Label className="text-base font-semibold" htmlFor="max-tokens">
-          Max Tokens
-        </Label>
-        <span className="w-12 rounded-md border border-transparent px-2 py-0.5 text-right text-sm text-muted-foreground hover:border-border">
-          {value}
-        </span>
-      </div>
+      <HoverCard openDelay={200}>
+        <HoverCardTrigger asChild>
+          <div className="flex items-center justify-between">
+            <Label className="text-base font-semibold" htmlFor="max-tokens">
+              Max Tokens
+            </Label>
+            <span className="w-12 rounded-md border border-transparent px-2 py-0.5 text-right text-sm text-muted-foreground hover:border-border">
+              {value}
+            </span>
+          </div>
+        </HoverCardTrigger>
+        <HoverCardContent align="start" side="left" className="w-64 text-sm">
+          The maximum number of tokens that can be generated in the chat completion. The total
+          length of input tokens and generated tokens is limited by the model's context length.
+        </HoverCardContent>
+      </HoverCard>
       <Slider
         id="top-p"
         defaultValue={value}
--- frontend/components/temperature-selector.tsx
+++ frontend/components/temperature-selector.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card';
 import { Label } from '@/components/ui/label';
 import { Slider } from '@/components/ui/slider';
 import { useParamStore } from '@/lib/param-store';
@@ -18,14 +19,23 @@ export default function TemperatureSelector() {
 
   return (
     <div className="flex flex-col space-y-2 lg:max-w-[250px] xl:max-w-[400px]">
-      <div className="flex items-center justify-between">
-        <Label className="text-base font-semibold" htmlFor="temperature">
-          Temperature
-        </Label>
-        <span className="w-12 rounded-md border border-transparent px-2 py-0.5 text-right text-sm text-muted-foreground hover:border-border">
-          {value}
-        </span>
-      </div>
+      <HoverCard openDelay={200}>
+        <HoverCardTrigger asChild>
+          <div className="flex items-center justify-between">
+            <Label className="text-base font-semibold" htmlFor="temperature">
+              Temperature
+            </Label>
+            <span className="w-12 rounded-md border border-transparent px-2 py-0.5 text-right text-sm text-muted-foreground hover:border-border">
+              {value}
+            </span>
+          </div>
+        </HoverCardTrigger>
+        <HoverCardContent align="start" side="left" className="w-64 text-sm">
+          What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the
+          output more random, while lower values like 0.2 will make it more focused and
+          deterministic.
+        </HoverCardContent>
+      </HoverCard>
       <Slider
         id="temperature"
         defaultValue={value}
